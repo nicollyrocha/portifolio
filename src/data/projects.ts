90ff18e0a5d2e97f81390ae342d492f9ad5e7950
Nicolly Rocha <nicollyrcs.dev@gmail.com>
2026-03-30 20:27:12 -0300

    adicionando projetos

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,14 +1,32 @@
 export const projects = [
-	{
-		name: 'Loja Virtual para teste de processo seletivo',
-		nameEnglish: 'Virtual store for a test from a process selection',
-		link: '',
-		github: 'https://github.com/nicollyrocha/mks-sistemas-loja',
-	},
-	{
-		name: 'Lista de Heróis',
-		nameEnglish: 'Heroes List',
-		link: '',
-		github: 'https://github.com/nicollyrocha/heroes-list',
-	},
+  {
+    name: "Loja Virtual para teste de processo seletivo",
+    nameEnglish: "Virtual store for a test from a process selection",
+    link: "",
+    github: "https://github.com/nicollyrocha/mks-sistemas-loja",
+  },
+  {
+    name: "Lista de Heróis",
+    nameEnglish: "Heroes List",
+    link: "",
+    github: "https://github.com/nicollyrocha/heroes-list",
+  },
+  {
+    name: "Sistema SAAS de Mensageria",
+    nameEnglish: "SaaS Messaging System",
+    link: "https://sendflow-test-533e0.web.app/",
+    github: "https://github.com/nicollyrocha/sendflow-test",
+  },
+  {
+    name: "Feed de Posts",
+    nameEnglish: "Post Feed",
+    link: "https://codeleap-frontend-test-beta.vercel.app/",
+    github: "https://github.com/nicollyrocha/codeleap-frontend-test",
+  },
+  {
+    name: "Caixa de entrada inteligente",
+    nameEnglish: "Smart Inbox",
+    link: "https://smart-inbox-ui.vercel.app/",
+    github: "https://github.com/nicollyrocha/smart-inbox-ui",
+  },
 ];
